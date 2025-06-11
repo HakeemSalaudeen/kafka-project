33a81f3f6ac2859a36eb9d1f1a1cc78e17c6b35c
Hakeem Oluwatobi <Hakeemsalaudeen01@gmail.com>
2025-06-11 03:30:37 +0100

    kafka done v2

--- kafka/ssm-boto3.py
+++ kafka/ssm-boto3.py
@@ -2,17 +2,35 @@ import boto3
 
 ssm_client = boto3.client('ssm')
 
-# Fetching parameters from AWS Systems Manager Parameter Store
-confluent_cluster_api_key = ssm_client.get_parameter(
+# Fetch parameters from SSM
+api_key = ssm_client.get_parameter(
     Name="/kafka/confluent_cluster_api_key",
     WithDecryption=True
-)
-confluent_cluster_api_secret = ssm_client.get_parameter(
+)['Parameter']['Value']
+
+api_secret = ssm_client.get_parameter(
     Name="/kafka/confluent_cluster_api_secret",
     WithDecryption=True
-)
-# Fetching parameters for Confluent Cloud bootstrap servers
-confluent_bootstrap_servers = ssm_client.get_parameter(
+)['Parameter']['Value']
+
+bootstrap_servers = ssm_client.get_parameter(
     Name="/kafka/confluent_bootstrap_servers",
     WithDecryption=True
-)
+)['Parameter']['Value']
+
+# Read existing config.properties
+config_path = "kafka/config.properties"
+with open(config_path, "r") as f:
+    lines = f.readlines()
+
+# Update relevant lines
+with open(config_path, "w") as f:
+    for line in lines:
+        if line.startswith("sasl.username="):
+            f.write(f"sasl.username={api_key}\n")
+        elif line.startswith("sasl.password="):
+            f.write(f"sasl.password={api_secret}\n")
+        elif line.startswith("bootstrap.servers="):
+            f.write(f"bootstrap.servers={bootstrap_servers}\n")
+        else:
+            f.write(line)
